a1cac6262b51ff77983adf959885bc8d1bb8cce6
libraryaddict <github@lib.co.nz>
2022-06-22 17:30:35 +1200

    Basic fix for cold res

--- src/quests/council/icepeak/QuestL8MountainBoss.ts
+++ src/quests/council/icepeak/QuestL8MountainBoss.ts
@@ -36,7 +36,7 @@ export class QuestL8MountainBoss implements QuestInfo {
   }
 
   run(): QuestAdventure {
-    let outfit = new GreyOutfit().addBonus("+2 cold res");
+    let outfit = new GreyOutfit().addBonus("+cold res 5 min 5 max");
 
     return {
       location: this.peak,
