9f09ba39dd7fda35e475e760703da671186d3bca
Build Script <>
2022-08-23 14:46:18 +0000

    Build Mafia files for commit e87b7c82cc3a95fbf2df484d06732a0be0a4fff9

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -1370,7 +1370,8 @@ var cosplaySaber = external_kolmafia_namespaceObject.Item.get("Fourth of May Cos
 var cosplayYellowRay = {
   type: ResourceCategory.YELLOW_RAY,
   id: "Cosplay Saber",
-  worthInAftercore: 3000, // Garbo has some use of it, but if you have an oflaction like its basically worth grimace pill/2 free fights
+  // At 4, it's worth 3k. 3 = 2k, 2 = 1k, 1/0 = 500 meat
+  worthInAftercore: Math.max(0.5, Math.min(3, (0,external_kolmafia_namespaceObject.modifierEval)("G") - 1)) * 1000, // Garbo has some use of it, but if you have an oflaction like its basically worth grimace pill/2 free fights
   prepare: (outfit, props) => {
     if (outfit != null) {
       outfit.addItem(cosplaySaber);
@@ -11425,13 +11426,21 @@ var QuestL12FratOutfit = /*#__PURE__*/function (_TaskInfo) {QuestL12FratOutfit_i
           props.setChoice(146, 2); // Items
 
           try {
-            var yr = path.getResource(ResourceCategory.YELLOW_RAY);
             var settings = new AdventureSettings();
 
-            if (yr != null) {
-              yr.prepare(null, props);
+            if (yellowRay != null) {
+              yellowRay.prepare(null, props);
               settings.setStartOfFightMacro(
-              Macro.if_("monstername Hippy", yr.macro()));
+              Macro.if_("monstername Hippy", yellowRay.macro()));
+
+              settings.setStartOfFightMacro(
+              Macro.if_("monstername War Pledge", yellowRay.macro()));
+
+              settings.setStartOfFightMacro(
+              Macro.if_(
+              "monstername Frat Warrior drill sergeant",
+              yellowRay.macro()));
+
 
             }
 
@@ -21830,7 +21839,7 @@ var QuestInitialPulls = /*#__PURE__*/function (_TaskInfo) {QuestInitialPulls_inh
         this.possiblePulls = this.possiblePulls.filter(
         (_ref3) => {var _ref4 = QuestInitialPulls_slicedToArray(_ref3, 1),i = _ref4[0];return (
             (0,external_kolmafia_namespaceObject.availableAmount)(i) == 0 && (
-            (0,external_kolmafia_namespaceObject.historicalPrice)(i) < 1000000 || (0,external_kolmafia_namespaceObject.storageAmount)(i) > 0));});
+            (0,external_kolmafia_namespaceObject.historicalPrice)(i) < 50000 || (0,external_kolmafia_namespaceObject.storageAmount)(i) > 0));});
 
       }
 
@@ -27008,7 +27017,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "bbd5826";
+var lastCommitHash = "e87b7c8";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
