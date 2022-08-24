318b9fc2a2b32e2f2a45e2f792676cddb713ebf5
Build Script <>
2022-08-24 12:49:49 +0000

    Build Mafia files for commit 4545b3b77202dbaaffd386143c896fe54a47f28c

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -12575,6 +12575,7 @@ var QuestL12Worms = /*#__PURE__*/function (_TaskInfo) {QuestL12Worms_inherits(Qu
           var path = new PossiblePath(
           combo.map((_ref) => {var _ref2 = QuestL12Worms_slicedToArray(_ref, 2),turns = _ref2[1];return turns;}).reduce((p, n) => p + n, 1));
 
+          path.addIgnored("Cosplay Saber");
 
           combo.forEach((_ref3) => {var _ref4 = QuestL12Worms_slicedToArray(_ref3, 1),resource = _ref4[0];
             if (resource == null) {
@@ -16479,6 +16480,7 @@ function QuestL6FriarExp_classCallCheck(instance, Constructor) {if (!(instance i
 
 
 
+
 var QuestL6FriarExp = /*#__PURE__*/function () {function QuestL6FriarExp() {QuestL6FriarExp_classCallCheck(this, QuestL6FriarExp);QuestL6FriarExp_defineProperty(this, "fam",
     external_kolmafia_namespaceObject.Familiar.get("Grey Goose"));}QuestL6FriarExp_createClass(QuestL6FriarExp, [{ key: "getId", value:
 
@@ -16513,6 +16515,7 @@ var QuestL6FriarExp = /*#__PURE__*/function () {function QuestL6FriarExp() {Ques
     function run() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.cliExecute)("friars familiar");
         } };
@@ -16744,6 +16747,7 @@ function QuestL6Friars_toConsumableArray(arr) {return QuestL6Friars_arrayWithout
 
 
 
+
 var QuestL6Friar = /*#__PURE__*/function () {function QuestL6Friar() {QuestL6Friars_classCallCheck(this, QuestL6Friar);QuestL6Friars_defineProperty(this, "woods",
     [
     new QuestL6FriarElbow(),
@@ -16787,6 +16791,7 @@ var QuestL6Friar = /*#__PURE__*/function () {function QuestL6Friar() {QuestL6Fri
     function run() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)("friars.php?action=ritual&pwd");
           (0,external_kolmafia_namespaceObject.visitUrl)("pandamonium.php");
@@ -27040,7 +27045,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "14e287c";
+var lastCommitHash = "4545b3b";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
@@ -27140,7 +27145,7 @@ GreyYouMain = /*#__PURE__*/function () {function GreyYouMain() {GreyYouMain_clas
       }
 
       (0,external_kolmafia_namespaceObject.print)(
-      "run <Turns> - Run X amount of turns, or 1 if turns are not provided",
+      "run <Turns> - Run X amount of turns, or 999 if turns are not provided",
       color);
 
       (0,external_kolmafia_namespaceObject.print)(
@@ -27304,7 +27309,7 @@ GreyYouMain = /*#__PURE__*/function () {function GreyYouMain() {GreyYouMain_clas
         this.adventures.runTurn(false);
         return;
       } else if (s[0] == "go" || s[0] == "run") {
-        var turns = (0,external_kolmafia_namespaceObject.toInt)(s[1] || "1");
+        var turns = (0,external_kolmafia_namespaceObject.toInt)(s[1] || "999");
 
         var effect = external_kolmafia_namespaceObject.Effect.get("Beaten Up");
         var lastBeaten = (0,external_kolmafia_namespaceObject.haveEffect)(effect);
