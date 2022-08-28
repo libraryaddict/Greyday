80d2547d34df57ae5a9ff0d900235bb1bb216452
Build Script <>
2022-08-28 05:38:05 +0000

    Build Mafia files for commit ed767037fb2663f577e562cc3b45f1cacd840aba

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -12880,6 +12880,7 @@ var QuestL12Worms = /*#__PURE__*/function (_TaskInfo) {QuestL12Worms_inherits(Qu
         _outfit.addItem(external_kolmafia_namespaceObject.Item.get("Beer Helmet"));
         _outfit.addItem(external_kolmafia_namespaceObject.Item.get("distressed denim pants"));
         _outfit.addItem(external_kolmafia_namespaceObject.Item.get("bejeweled pledge pin"));
+        _outfit.addBonus("-tie");
 
         return {
           outfit: _outfit,
@@ -17935,7 +17936,8 @@ var QuestL8MountainOre = /*#__PURE__*/function (_TaskInfo) {QuestL8MountainOre_i
           2 :
           res == ResourceCategory.PULL ||
           res == ResourceCategory.COPIER ||
-          res == ResourceCategory.CLOVER ?
+          res == ResourceCategory.CLOVER ||
+          res == ResourceCategory.CAT_HEIST ?
           1 :
           0).
 
@@ -20338,6 +20340,7 @@ function QuestManor_classCallCheck(instance, Constructor) {if (!(instance instan
 
 
 
+
 var QuestManor = /*#__PURE__*/function () {function QuestManor() {QuestManor_classCallCheck(this, QuestManor);QuestManor_defineProperty(this, "quests",
     [
     new ManorBathroom(),
@@ -20411,6 +20414,7 @@ var QuestManor = /*#__PURE__*/function () {function QuestManor() {QuestManor_cla
 
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)("place.php?whichplace=manor2&action=manor2_ladys");
           greyAdv(this.ballroom);
@@ -20430,6 +20434,7 @@ var QuestManor = /*#__PURE__*/function () {function QuestManor() {QuestManor_cla
     function doUpstairs() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.print)("Lets chat up the old lady");
           (0,external_kolmafia_namespaceObject.visitUrl)("place.php?whichplace=manor1&action=manor1_ladys");
@@ -23650,7 +23655,8 @@ var QuestSkillConiferPolymers = /*#__PURE__*/function () {function QuestSkillCon
     } }, { key: "status", value:
 
     function status() {
-      if ((0,external_kolmafia_namespaceObject.haveSkill)(this.skill)) {
+      //if (haveSkill(this.skill))
+      {
         return QuestStatus.COMPLETED;
       }
 
@@ -24777,11 +24783,13 @@ var QuestRegistry = /*#__PURE__*/function () {
 
       { id: "Council / War / Gremlins" },
 
-      { id: "Council / War / Battlefield" },
       { id: "Council / War / Boss" },
 
       // Tavern needs Larva done
       { id: "Council / Tavern" },
+
+      { id: "Council / War / Battlefield" },
+
       { id: "Council / MacGruffin / Pyramid / EdUndying" },
 
       // Alright, this run is just about over kids. Lets finish it.
@@ -27283,7 +27291,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "460ebee";
+var lastCommitHash = "ed76703";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
