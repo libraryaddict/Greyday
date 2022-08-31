d6c165e922494cd2f36096fc702caad1676ccb5e
Build Script <>
2022-08-31 06:56:45 +0000

    Build Mafia files for commit 2ead89ad807e6d7f1e8f0ce691faea6fab9425ff

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -8030,6 +8030,10 @@ var QuestTrapGhost = /*#__PURE__*/function () {function QuestTrapGhost() {QuestT
 
     function getLocations() {
       return [];
+    } }, { key: "mustBeDone", value:
+
+    function mustBeDone() {
+      return true;
     } }]);return QuestTrapGhost;}();
 
 
@@ -11003,7 +11007,7 @@ var QuestL11TempleUnlock = /*#__PURE__*/function () {function QuestL11TempleUnlo
             return;
           }
 
-          var settings = new AdventureSettings().setChoices(this.choices);
+          var settings = new AdventureSettings();
 
           if (isGhostBustingTime(this.spookyLoc)) {
             settings.setStartOfFightMacro(getGhostBustingMacro());
@@ -11022,6 +11026,8 @@ var QuestL11TempleUnlock = /*#__PURE__*/function () {function QuestL11TempleUnlo
           }
 
           this.runSpookyChoices();
+
+          settings.setChoices(this.choices);
 
           greyAdv(this.spookyLoc, outfit, settings);
 
@@ -23037,6 +23043,7 @@ QuestDruggie = /*#__PURE__*/function () {function QuestDruggie() {QuestNpcStuff_
     function run() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)("place.php?whichplace=woods");
           (0,external_kolmafia_namespaceObject.visitUrl)("tavern.php?place=susguy&action=buygoofballs", true);
@@ -23184,6 +23191,7 @@ QuestArtist = /*#__PURE__*/function () {function QuestArtist() {QuestNpcStuff_cl
     function startQuest() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)(
           "place.php?whichplace=town_wrong&action=townwrong_artist_noquest");
@@ -23201,6 +23209,7 @@ QuestArtist = /*#__PURE__*/function () {function QuestArtist() {QuestNpcStuff_cl
     function turnInQuest() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)(
           "place.php?whichplace=town_wrong&action=townwrong_artist_quest");
@@ -23258,6 +23267,7 @@ QuestMadBaker = /*#__PURE__*/function () {function QuestMadBaker() {QuestNpcStuf
     function turnInPie() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)("shop.php?whichshop=armory");
           (0,external_kolmafia_namespaceObject.visitUrl)("choice.php?whichchoice=" + (0,external_kolmafia_namespaceObject.lastChoice)() + "&option=2&pwd=");
@@ -23268,6 +23278,7 @@ QuestMadBaker = /*#__PURE__*/function () {function QuestMadBaker() {QuestNpcStuf
     function startQuest() {
       return {
         location: null,
+        outfit: GreyOutfit.IGNORE_OUTFIT,
         run: () => {
           (0,external_kolmafia_namespaceObject.visitUrl)("shop.php?whichshop=armory");
           (0,external_kolmafia_namespaceObject.visitUrl)("shop.php?whichshop=armory&action=talk");
@@ -23324,6 +23335,7 @@ QuestUntinker = /*#__PURE__*/function () {function QuestUntinker() {QuestNpcStuf
       if ((0,external_kolmafia_namespaceObject.getProperty)("questM01Untinker") == "unstarted") {
         return {
           location: null,
+          outfit: GreyOutfit.IGNORE_OUTFIT,
           run: () => {
             (0,external_kolmafia_namespaceObject.visitUrl)(
             "place.php?whichplace=forestvillage&preaction=screwquest&action=fv_untinker_quest");
@@ -23336,6 +23348,7 @@ QuestUntinker = /*#__PURE__*/function () {function QuestUntinker() {QuestNpcStuf
       if ((0,external_kolmafia_namespaceObject.knollAvailable)() && (0,external_kolmafia_namespaceObject.availableAmount)(this.item) == 0) {
         return {
           location: null,
+          outfit: GreyOutfit.IGNORE_OUTFIT,
           run: () => {
             (0,external_kolmafia_namespaceObject.visitUrl)("place.php?whichplace=knoll_friendly&action=dk_innabox");
           } };
@@ -23364,6 +23377,7 @@ QuestMeatSmith = /*#__PURE__*/function () {function QuestMeatSmith() {QuestNpcSt
       if ((0,external_kolmafia_namespaceObject.getProperty)("questM23Meatsmith") == "unstarted") {
         return {
           location: null,
+          outfit: GreyOutfit.IGNORE_OUTFIT,
           run: () => {
             (0,external_kolmafia_namespaceObject.visitUrl)("shop.php?whichshop=meatsmith");
             (0,external_kolmafia_namespaceObject.visitUrl)("shop.php?whichshop=meatsmith&action=talk");
@@ -27618,7 +27632,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "7069848";
+var lastCommitHash = "2ead89a";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
