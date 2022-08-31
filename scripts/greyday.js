1f55490a2822dc675315a5b2ed8c2a6f48c1824f
Build Script <>
2022-08-31 11:28:56 +0000

    Build Mafia files for commit 03b5972b5908f95bc49557876188e414320c6bb9

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -3017,6 +3017,12 @@ var GreyRequirements = /*#__PURE__*/function () {function GreyRequirements() {Gr
 
 
       add(
+      external_kolmafia_namespaceObject.Item.get("Fourth of May Cosplay Saber"),
+      "Great for faster lobsterfrogmen, ele res checks and for 1-2 yellow rays!",
+      Required.USEFUL);
+
+
+      add(
       external_kolmafia_namespaceObject.Item.get("protonic accelerator pack"),
       "Used for some free delay, -5 combat, get a nice +2 fam exp offhand and as a MP restorer! With sweatpants as a stunner, this is great!",
       Required.VERY_USEFUL);
@@ -3160,14 +3166,14 @@ var GreyRequirements = /*#__PURE__*/function () {function GreyRequirements() {Gr
 
       add(
       "Gingerbread City",
-      "Useful as another key source - CURRENTLY UNSUPPORTED",
+      "Useful as another key source - <b><u>CURRENTLY UNSUPPORTED</u></b>",
       Required.MINOR,
       (0,external_kolmafia_namespaceObject.toBoolean)((0,external_kolmafia_namespaceObject.getProperty)("gingerbreadCityAvailable")));
 
 
       add(
       "Piraterealm",
-      "Useful as another key source - CURRENTLY UNSUPPORTED",
+      "Useful as another key source - <b><u>CURRENTLY UNSUPPORTED</u></b>",
       Required.MINOR,
       (0,external_kolmafia_namespaceObject.toBoolean)((0,external_kolmafia_namespaceObject.getProperty)("prAlways")));
 
@@ -3187,7 +3193,7 @@ var GreyRequirements = /*#__PURE__*/function () {function GreyRequirements() {Gr
 
       add(
       external_kolmafia_namespaceObject.Item.get("Greatest American Pants"),
-      "Free runs - CURRENTLY UNSUPPORTED",
+      "Free runs - <b><u>CURRENTLY UNSUPPORTED</u></b>",
       Required.USEFUL);
 
 
@@ -27638,7 +27644,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "78a5d08";
+var lastCommitHash = "03b5972";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
