84b8f1b3a870b59abf1de9bcc9d9ac534e1af067
Build Script <>
2022-10-01 06:46:32 +0000

    Build Mafia files for commit 0c98da6daef71f55f47eb435b48a548d6656242a

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -13657,7 +13657,7 @@ var QuestL12StartWar = /*#__PURE__*/function (_TaskInfo) {QuestL12StartWar_inher
       var nc = path.getResource(ResourceCategory.FORCE_NC);
 
       if (nc == null) {
-        outfit.setNoCombat();
+        outfit.setNoCombat().setNoCombat();
 
         if (
         (0,external_kolmafia_namespaceObject.availableAmount)(this.umbrella) > 0 &&
@@ -14118,13 +14118,16 @@ GremlinQuest = /*#__PURE__*/function () {
       Macro.while_("!pastround 25 && !hpbelow 50", macro2));
 
 
+      var orbs = [
+      this.monster,
+      external_kolmafia_namespaceObject.Monster.get(this.monster.name.replace(" (tool)", ""))];
+
+
       return {
         location: this.loc,
         outfit: outfit,
-        orbs: [
-        this.monster,
-        external_kolmafia_namespaceObject.Monster.get(this.monster.name.replace(" (tool)", ""))],
-
+        orbs: orbs,
+        mayFreeRun: true,
         freeRun: (mons) => mons != this.monster,
         run: () => {
           var settings = new AdventureSettings();
@@ -14191,14 +14194,18 @@ var QuestL12Lobster = /*#__PURE__*/function (_TaskInfo) {QuestL12WarLobster_inhe
       var turnsManual = 8;
       var possibleCombo = [];
 
-      for (
-      var i = 0;
-      i < barrelsNeeded - (assumeUnstarted ? 0 : this.getFriendsRemaining());
-      i++)
-      {
-        possibleCombo.push("Manual");
+      for (var i = 0; i < barrelsNeeded; i++) {
+        if (i < this.getFriendsRemaining()) {
+          possibleCombo.push("Friend");
+        } else {
+          possibleCombo.push("Manual");
+        }
+
         possibleCombo.push(ResourceCategory.COPIER);
-        possibleCombo.push(ResourceCategory.OLFACT_COPIER);
+
+        if (i <= 1) {
+          possibleCombo.push(ResourceCategory.OLFACT_COPIER);
+        }
       }
 
       // If we're doing voted, or mag class. Then we can do a replace
@@ -14249,7 +14256,10 @@ var QuestL12Lobster = /*#__PURE__*/function (_TaskInfo) {QuestL12WarLobster_inhe
           var barrelsGained = 0;var _iterator2 = QuestL12WarLobster_createForOfIteratorHelper(
 
           combo),_step2;try {for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {var type = _step2.value;
-              if (type == "Manual") {
+              if (type == "Friend") {
+                turnsTaken += 1;
+                barrelsGained += 1;
+              } else if (type == "Manual") {
                 turnsTaken += turnsManual;
                 barrelsGained++;
               } else if (type == ResourceCategory.COPIER) {
@@ -14679,6 +14689,10 @@ var QuestL12Lobster = /*#__PURE__*/function (_TaskInfo) {QuestL12WarLobster_inhe
           return true;
         }
 
+        return false;
+      }
+
+      if (this.getFriendsRemaining() > 0) {
         return false;
       }
 
@@ -17123,7 +17137,7 @@ var QuestTowerWallBones = /*#__PURE__*/function () {function QuestTowerWallBones
     } }, { key: "runKnife", value:
 
     function runKnife() {
-      var outfit = new GreyOutfit().setNoCombat();
+      var outfit = new GreyOutfit().setNoCombat().setNoCombat();
 
       return {
         location: this.groundFloor,
@@ -31063,7 +31077,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "c178a5a";
+var lastCommitHash = "0c98da6";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
