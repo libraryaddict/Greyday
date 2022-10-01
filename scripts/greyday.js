47aff56a061a5b014504063d155056375b9efc0f
Build Script <>
2022-10-01 06:14:27 +0000

    Build Mafia files for commit a72c65818f1437b8fbc2b0d5ce8f1eb35c74630d

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -2797,8 +2797,12 @@ var GreyOutfit = /*#__PURE__*/function () {
 
 
 
+  /**
+   * When we don't want to cap how much combat we run
+   */
 
-  function GreyOutfit() {var string = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;GreyOutfitter_classCallCheck(this, GreyOutfit);GreyOutfitter_defineProperty(this, "allowChampBottle", false);GreyOutfitter_defineProperty(this, "famExpWeight", 30);GreyOutfitter_defineProperty(this, "itemDropWeight", 0.3);GreyOutfitter_defineProperty(this, "meatDropWeight", 0.1);GreyOutfitter_defineProperty(this, "hpWeight", 0.001);GreyOutfitter_defineProperty(this, "hpRegenWeight", 0.01);GreyOutfitter_defineProperty(this, "mpWeight", 0.001);GreyOutfitter_defineProperty(this, "mpRegenWeight", 0.01);GreyOutfitter_defineProperty(this, "initWeight", 0.05);GreyOutfitter_defineProperty(this, "plusCombatWeight", 0);GreyOutfitter_defineProperty(this, "minusCombatWeight", 0);GreyOutfitter_defineProperty(this, "itemsWeight", []);GreyOutfitter_defineProperty(this, "bonusWeights", []);GreyOutfitter_defineProperty(this, "overrideMaximizer", void 0);GreyOutfitter_defineProperty(this, "umbrellaSetting", void 0);
+
+  function GreyOutfit() {var string = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;GreyOutfitter_classCallCheck(this, GreyOutfit);GreyOutfitter_defineProperty(this, "allowChampBottle", false);GreyOutfitter_defineProperty(this, "famExpWeight", 30);GreyOutfitter_defineProperty(this, "itemDropWeight", 0.3);GreyOutfitter_defineProperty(this, "meatDropWeight", 0.1);GreyOutfitter_defineProperty(this, "hpWeight", 0.001);GreyOutfitter_defineProperty(this, "hpRegenWeight", 0.01);GreyOutfitter_defineProperty(this, "mpWeight", 0.001);GreyOutfitter_defineProperty(this, "mpRegenWeight", 0.01);GreyOutfitter_defineProperty(this, "initWeight", 0.05);GreyOutfitter_defineProperty(this, "plusCombatWeight", 0);GreyOutfitter_defineProperty(this, "minusCombatWeight", 0);GreyOutfitter_defineProperty(this, "itemsWeight", []);GreyOutfitter_defineProperty(this, "bonusWeights", []);GreyOutfitter_defineProperty(this, "overrideMaximizer", void 0);GreyOutfitter_defineProperty(this, "umbrellaSetting", void 0);GreyOutfitter_defineProperty(this, "uncapped", false);
     this.overrideMaximizer = string;
 
     this.setWeights();
@@ -2885,11 +2889,19 @@ var GreyOutfit = /*#__PURE__*/function () {
     } }, { key: "setNoCombat", value:
 
     function setNoCombat() {
+      if (this.minusCombatWeight > 0) {
+        this.setUncapped();
+      }
+
       this.minusCombatWeight += 10;
       return this;
     } }, { key: "setPlusCombat", value:
 
     function setPlusCombat() {
+      if (this.plusCombatWeight > 0) {
+        this.setUncapped();
+      }
+
       this.plusCombatWeight += 10;
       return this;
     } }, { key: "setItemDrops", value:
@@ -2902,6 +2914,12 @@ var GreyOutfit = /*#__PURE__*/function () {
 
     function setChampagneBottle() {
       this.allowChampBottle = true;
+
+      return this;
+    } }, { key: "setUncapped", value:
+
+    function setUncapped() {
+      this.uncapped = true;
 
       return this;
     } }, { key: "createString", value:
@@ -2946,11 +2964,21 @@ var GreyOutfit = /*#__PURE__*/function () {
       }
 
       if (this.plusCombatWeight > 0) {
-        modifiers.push("+" + this.plusCombatWeight + " combat 25 MAX");
+        modifiers.push(
+        "+" +
+        this.plusCombatWeight +
+        " combat" + (
+        this.uncapped ? "" : " 25 MAX"));
+
       }
 
       if (this.minusCombatWeight > 0) {
-        modifiers.push("-" + this.minusCombatWeight + " combat 25 MAX");
+        modifiers.push(
+        "-" +
+        this.minusCombatWeight +
+        " combat" + (
+        this.uncapped ? "" : " 25 MAX"));
+
       }var _iterator = GreyOutfitter_createForOfIteratorHelper(
 
       this.itemsWeight),_step;try {for (_iterator.s(); !(_step = _iterator.n()).done;) {var pair = _step.value;
@@ -31033,7 +31061,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "c99f844";
+var lastCommitHash = "a72c658";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
