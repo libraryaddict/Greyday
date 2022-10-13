b0b566fbf1e92d5e6384a655b058efe94a721517
libraryaddict <github@lib.co.nz>
2022-10-13 20:54:16 +1300

    ..

--- src/GreyToDo.ts
+++ src/GreyToDo.ts
@@ -3,16 +3,12 @@
 // Calculate if we should melee or spell damage properly for orcs
 // Improve aboo checking, including if we can finish all the thing before trying to go there.
 // Use other banishers
-// Use backups in quest zones
 // Consider doing filthworms early if we have nanovision and need to burn turns
 // Use other familiars if we can
 // When we're trying to hit item drop, don't try to level up other fams
 // Use emit dupes more? Would require better planning for absorbs though
-// Possibly delay kitchen until we can prove we have enough resist or we're too far ahead that this is going to slow us down
 // Support more worksheds
 // Maybe use daylight shavings buffs somehow
-// Use candles and other potions
-// Use pocket professor copier?
 // Use melodramy copiers maybe? May need to pull helmet
 // Maybe use the vampire cloak, esp for nuns and maybe kitchen
 // Use doctor's bag specials?
@@ -27,12 +23,9 @@
 // Potentially try to unlock dungeons of doom easier / absorb +mp mobs
 // spooky putty? Forgot what it does
 // Maybe stuffing fluffers, look at loopcasual to see what dirty tricks it pulls
-// Use kramco when its ready and there's no zones that wants it
 // Pull some source of infinite free runs and run whenever it's a fight we don't care about
 // Level up camel when we can, which if we're running lots of +fam exp, is often. Delay zones that'd like that spit. Mountain Man, Middle Pyramid, Goat Cheese, Lobster, sonar?, giants for their mohawk/amulet?, enchanted bean?, stephen?, stars, white pixel, evil eye, funky junk key?, filth worms glands?, bowler?, glark cables?, cig lighter?, crappy disguise?, stone wool?, bitchin car?, knob perfume? Rusty hedge trimmers
 // Adjust familiar weight bonus based on how soon we're going to need it
 // Smart banishers, such as some method to get number of available banishers and if banishers will be available in the future.
-// Create some kind of earmark system where quests/tasks can preregister what resources/meat they need. So I can burn resources on optional stuff without worrying about interfering with something else
 
 // Convert quest status to 'READY' 'NOT READY' 'COMPLETED'. And add subquest status to 'READY' where it can declare a priority over other stuff.
-// Use crystal ball to fish for non-absorbs monsters that we want and where spending turns in the zone benifits no one. Such as blur, the +meat plant. We can switch to other zones that want progression and cost us nothing in resources to spend away
