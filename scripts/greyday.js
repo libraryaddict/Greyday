10ed6208348190649a0a65b93b44b8949195130e
Build Script <>
2022-10-16 22:36:54 +0000

    Build Mafia files for commit e917d557cd1b3deb07c5762444edc2af562c7363

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -10041,7 +10041,7 @@ var QuestL11RonProtesters = /*#__PURE__*/function (_TaskInfo) {QuestL11RonProtes
       "Clover"]);
 
 
-      if ((0,external_kolmafia_namespaceObject.availableAmount)(this.spoon) == 0 && !(0,external_kolmafia_namespaceObject.gnomadsAvailable)()) {
+      if (!this.willHaveShirt()) {
         allPossible = allPossible.filter((i) => i != this.lyrndShirt);
       }var _iterator = QuestL11RonProtesters_createForOfIteratorHelper(
 
@@ -10187,16 +10187,19 @@ var QuestL11RonProtesters = /*#__PURE__*/function (_TaskInfo) {QuestL11RonProtes
         (0,external_kolmafia_namespaceObject.getProperty)("questL11Ron") == "step1" ||
         (0,external_kolmafia_namespaceObject.toInt)((0,external_kolmafia_namespaceObject.getProperty)("zeppelinProtestors")) <= 80);
 
-    } }, { key: "waitingForShirt", value:
+    } }, { key: "willHaveShirt", value:
 
-    function waitingForShirt() {
+    function willHaveShirt() {
       return (
-        !(0,external_kolmafia_namespaceObject.haveSkill)(this.torsoAwareness) && (
         (0,external_kolmafia_namespaceObject.gnomadsAvailable)() ||
         (0,external_kolmafia_namespaceObject.availableAmount)(this.spoon) > 0 &&
         getMoonZone(GreySettings.greyTuneMoonSpoon) == "Gnomad" &&
-        (0,external_kolmafia_namespaceObject.getProperty)("moonTuned") != "true"));
+        (0,external_kolmafia_namespaceObject.getProperty)("moonTuned") != "true");
 
+    } }, { key: "waitingForShirt", value:
+
+    function waitingForShirt() {
+      return !(0,external_kolmafia_namespaceObject.haveSkill)(this.torsoAwareness) && this.willHaveShirt();
     } }, { key: "getId", value:
 
     function getId() {
@@ -32265,7 +32268,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "8b94c04";
+var lastCommitHash = "e917d55";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
