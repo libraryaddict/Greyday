3a5bebd24370782f77fbe3dc41d1b91ef4b68e1a
Build Script <>
2022-10-21 23:56:34 +0000

    Build Mafia files for commit f9703575de79368002bfa614f17f9c615399853e

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -27135,7 +27135,8 @@ var QuestLocketInfiniteLoop = /*#__PURE__*/function (_TaskInfo) {QuestLocketInfi
     external_kolmafia_namespaceObject.Item.get("Flame orb"));QuestLocketInfiniteLoop_defineProperty(QuestLocketInfiniteLoop_assertThisInitialized(_this), "wish",
     external_kolmafia_namespaceObject.Item.get("Pocket Wish"));QuestLocketInfiniteLoop_defineProperty(QuestLocketInfiniteLoop_assertThisInitialized(_this), "pantsgiving",
     external_kolmafia_namespaceObject.Item.get("Pantsgiving"));QuestLocketInfiniteLoop_defineProperty(QuestLocketInfiniteLoop_assertThisInitialized(_this), "doctorsBag",
-    external_kolmafia_namespaceObject.Item.get("Lil' Doctor&trade; bag"));QuestLocketInfiniteLoop_defineProperty(QuestLocketInfiniteLoop_assertThisInitialized(_this), "paths", void 0);return _this;}QuestLocketInfiniteLoop_createClass(QuestLocketInfiniteLoop, [{ key: "level", value:
+    external_kolmafia_namespaceObject.Item.get("Lil' Doctor&trade; bag"));QuestLocketInfiniteLoop_defineProperty(QuestLocketInfiniteLoop_assertThisInitialized(_this), "cape",
+    external_kolmafia_namespaceObject.Item.get("Unwrapped knock-off retro superhero cape"));QuestLocketInfiniteLoop_defineProperty(QuestLocketInfiniteLoop_assertThisInitialized(_this), "paths", void 0);return _this;}QuestLocketInfiniteLoop_createClass(QuestLocketInfiniteLoop, [{ key: "level", value:
 
 
     function level() {
@@ -27210,6 +27211,10 @@ var QuestLocketInfiniteLoop = /*#__PURE__*/function (_TaskInfo) {QuestLocketInfi
       outfit.hpRegenWeight = 1;
       outfit.mpRegenWeight = 1;
 
+      if ((0,external_kolmafia_namespaceObject.availableAmount)(this.cape) > 0) {
+        outfit.addWeight(this.cape);
+      }
+
       if ((0,external_kolmafia_namespaceObject.availableAmount)(this.doctorsBag) > 0) {
         outfit.addWeight(this.doctorsBag);
       } else if ((0,external_kolmafia_namespaceObject.availableAmount)(this.pantsgiving) > 0) {
@@ -27222,7 +27227,9 @@ var QuestLocketInfiniteLoop = /*#__PURE__*/function (_TaskInfo) {QuestLocketInfi
         location: null,
         outfit: outfit,
         run: () => {
-          if ((0,external_kolmafia_namespaceObject.numericModifier)("Initiative") <= 40) {
+          if ((0,external_kolmafia_namespaceObject.equippedAmount)(this.cape) > 0) {
+            (0,external_kolmafia_namespaceObject.cliExecute)("retrocape heck hold"); // Make sure we try a stunner
+          } else if ((0,external_kolmafia_namespaceObject.numericModifier)("Initiative") <= 40) {
             throw "Initiative is 40 or less, aborting to be safe as we can't be sure you'd get the jump.";
           }
 
@@ -32528,7 +32535,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "d16f13e";
+var lastCommitHash = "f970357";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
