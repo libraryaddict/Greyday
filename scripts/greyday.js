1935df3aba4abad3289570e039943a6d904aad43
Build Script <>
2022-10-25 09:03:29 +0000

    Build Mafia files for commit 5ee0ae1934cc57912702f5da0398e00de4a09f24

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -12002,7 +12002,8 @@ var QuestL11Doctor = /*#__PURE__*/function () {function QuestL11Doctor() {QuestL
     map((s) => external_kolmafia_namespaceObject.Item.get(s)));QuestL11TempleDoctor_defineProperty(this, "loc",
     external_kolmafia_namespaceObject.Location.get("The Hidden Hospital"));QuestL11TempleDoctor_defineProperty(this, "surgeon",
     external_kolmafia_namespaceObject.Monster.get("pygmy witch surgeon"));QuestL11TempleDoctor_defineProperty(this, "shirt",
-    external_kolmafia_namespaceObject.Item.get("Surgical apron"));}QuestL11TempleDoctor_createClass(QuestL11Doctor, [{ key: "getLocations", value:
+    external_kolmafia_namespaceObject.Item.get("Surgical apron"));QuestL11TempleDoctor_defineProperty(this, "torso",
+    external_kolmafia_namespaceObject.Skill.get("Torso Awareness"));}QuestL11TempleDoctor_createClass(QuestL11Doctor, [{ key: "getLocations", value:
 
     function getLocations() {
       return [this.loc];
@@ -12039,6 +12040,10 @@ var QuestL11Doctor = /*#__PURE__*/function () {function QuestL11Doctor() {QuestL
       var seekingOutfit = false;var _iterator = QuestL11TempleDoctor_createForOfIteratorHelper(
 
         this.equips),_step;try {for (_iterator.s(); !(_step = _iterator.n()).done;) {var i = _step.value;
+          if (i == this.shirt && !(0,external_kolmafia_namespaceObject.haveSkill)(this.torso)) {
+            continue;
+          }
+
           if ((0,external_kolmafia_namespaceObject.availableAmount)(i) == 0) {
             seekingOutfit = true;
             continue;
@@ -30592,12 +30597,11 @@ var AdventureFinder = /*#__PURE__*/function () {
       var outfit = adventure.adventure.outfit;
 
       if (
-      outfit != null &&
-      outfit == GreyOutfit.IGNORE_OUTFIT
+      outfit != null && outfit == GreyOutfit.IGNORE_OUTFIT ||
       //outfit.plusCombatWeight > 0 ||
       //  outfit.minusCombatWeight > 0 ||
-      //   ||  outfit.extra.find((e) => e.startsWith("+equip")) != null
-      ) {
+      outfit.extra.find((e) => e.startsWith("+equip")) != null)
+      {
         return;
       }
 
@@ -32580,7 +32584,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "40a1b65";
+var lastCommitHash = "5ee0ae1";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
