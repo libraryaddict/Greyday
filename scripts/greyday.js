139e58428211e8eef6ac873270be85ec6243c11b
Build Script <>
2022-10-25 08:50:48 +0000

    Build Mafia files for commit 40a1b6561c893bd40a81200bfd10a57b6b8e1339

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -9494,7 +9494,7 @@ var QuestL11PalinBook = /*#__PURE__*/function (_TaskInfo) {QuestL11PalinBook_inh
 
       return {
         outfit: outfit,
-        location: this.palindome,
+        location: isGhostBustingTime(this.palindome) ? null : this.palindome,
         orbs: orbs,
         mayFreeRun: true,
         freeRun: (monster) => !orbs.includes(monster),
@@ -11717,6 +11717,7 @@ function QuestL11TempleCurses_classCallCheck(instance, Constructor) {if (!(insta
 
 
 
+
 var QuestL11Curses = /*#__PURE__*/function (_TaskInfo) {QuestL11TempleCurses_inherits(QuestL11Curses, _TaskInfo);var _super = QuestL11TempleCurses_createSuper(QuestL11Curses);function QuestL11Curses() {var _this;QuestL11TempleCurses_classCallCheck(this, QuestL11Curses);for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {args[_key] = arguments[_key];}_this = _super.call.apply(_super, [this].concat(args));QuestL11TempleCurses_defineProperty(QuestL11TempleCurses_assertThisInitialized(_this), "curse1",
     external_kolmafia_namespaceObject.Effect.get("Once-Cursed"));QuestL11TempleCurses_defineProperty(QuestL11TempleCurses_assertThisInitialized(_this), "curse2",
     external_kolmafia_namespaceObject.Effect.get("Twice-Cursed"));QuestL11TempleCurses_defineProperty(QuestL11TempleCurses_assertThisInitialized(_this), "curse3",
@@ -11797,9 +11798,16 @@ var QuestL11Curses = /*#__PURE__*/function (_TaskInfo) {QuestL11TempleCurses_inh
         return false;
       }
 
-      var delay = this.delayForNextNC() + 4;
+      var delay = this.delayForNextNC();
+      var hasGracePeriod = delay < turns;
+      var mayAsWellResetOrb =
+      hasGracePeriod && currentPredictions().has(this.apartment);
 
-      return turns <= delay;
+      if (!(0,external_kolmafia_namespaceObject.haveEffect)(this.curse3) && mayAsWellResetOrb) {
+        return false;
+      }
+
+      return turns <= delay + 4;
     } }, { key: "canRun", value:
 
     function canRun() {
@@ -12856,7 +12864,7 @@ var QuestL11TempleUnlock = /*#__PURE__*/function () {function QuestL11TempleUnlo
       }
 
       return {
-        location: this.spookyLoc,
+        location: isGhostBustingTime(this.spookyLoc) ? null : this.spookyLoc,
         outfit: outfit,
         freeRun: () => true,
         run: () => {
@@ -18617,7 +18625,7 @@ var QuestL2SpookyLarva = /*#__PURE__*/function () {function QuestL2SpookyLarva()
       }
 
       return {
-        location: this.location,
+        location: isGhostBustingTime(this.location) ? null : this.location,
         outfit: outfit,
         forcedFight: isGhostBustingTime(this.location) ?
         [0, this.headless] :
@@ -23585,7 +23593,7 @@ var ManorGallery = /*#__PURE__*/function () {function ManorGallery() {QuestManor
       outfit.setNoCombat();
 
       return {
-        location: this.location,
+        location: isGhostBustingTime(this.location) ? null : this.location,
         outfit: outfit,
         freeRun: () => true,
         run: () => {
@@ -32380,7 +32388,8 @@ var GreyAdventurer = /*#__PURE__*/function () {function GreyAdventurer() {GreyAd
             var buffer = (0,external_kolmafia_namespaceObject.fileToBuffer)(name);
 
             if (buffer == "") {
-              buffer = "# Turns Played\tQuest ID\tLocation\tTurns Taken";
+              buffer =
+              "# Turns Played\tQuest ID\tLocation\tTurns Taken\tFamiliar";
             }
 
             var id = adventure.quest ? adventure.quest.getId() : "Non-Quest";
@@ -32388,8 +32397,8 @@ var GreyAdventurer = /*#__PURE__*/function () {function GreyAdventurer() {GreyAd
             toRun.location;
 
             buffer += "\n".concat((0,external_kolmafia_namespaceObject.turnsPlayed)(), "\t").concat(id, "\t").concat(toRun.location, "\t").concat(
-            (0,external_kolmafia_namespaceObject.turnsPlayed)() - turn);
-
+            (0,external_kolmafia_namespaceObject.turnsPlayed)() - turn, "\t").concat(
+            (0,external_kolmafia_namespaceObject.myFamiliar)().name);
 
             (0,external_kolmafia_namespaceObject.bufferToFile)(buffer.toString(), name);
           }
@@ -32571,7 +32580,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "c2577cf";
+var lastCommitHash = "40a1b65";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
