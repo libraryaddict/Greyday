d72c9204e690b0e9e72be450673a4c1bce8d3e22
Build Script <>
2022-10-26 05:52:20 +0000

    Build Mafia files for commit 2993d9ad0af45ce9f372ad1ce3914ee75d7182c1

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -158,12 +158,14 @@ function getFax(monster) {
     throw "VIP was disabled, but function fax was still called";
   }
 
-  if (!canAccessClan(getDefaultClan()) || !(0,external_kolmafia_namespaceObject.isOnline)("cheesefax")) {
+  var faxbot = "CheeseFax";
+
+  if (!canAccessClan(getDefaultClan()) || !(0,external_kolmafia_namespaceObject.isOnline)(faxbot)) {
     throw (
       "Cannot access fax machine, clan accessible? " +
       canAccessClan(getDefaultClan()) +
       ". CheeseFax online? " +
-      (0,external_kolmafia_namespaceObject.isOnline)("cheesefax"));
+      (0,external_kolmafia_namespaceObject.isOnline)(faxbot));
 
   }
 
@@ -191,19 +193,28 @@ function getFax(monster) {
     };
 
     if (!hasReceivedFax()) {
-      (0,external_kolmafia_namespaceObject.chatPrivate)("cheesefax", monster.name);
+      for (var i = 0; i < 6; i++) {
+        // We might have missed it or overrode it
+        if (i % 3 == 0) {
+          (0,external_kolmafia_namespaceObject.chatPrivate)(faxbot, monster.name);
+        }
 
-      for (var i = 0; i < 3; i++) {
         (0,external_kolmafia_namespaceObject.wait)(10);
 
         if (hasReceivedFax()) {
-          break;
+          return;
         }
       }
+    }
 
-      if (!hasReceivedFax()) {
-        throw new Error("Failed to acquire photocopied " + monster);
-      }
+    if (!hasReceivedFax()) {
+      throw new Error(
+      "Failed to acquire photocopied " +
+      monster +
+      ", assuming " +
+      faxbot +
+      " is online you probably just missed the fax. Try running the script again.");
+
     }
   });
 }
@@ -21470,12 +21481,18 @@ var QuestL8MountainOre = /*#__PURE__*/function (_TaskInfo) {QuestL8MountainOre_i
     } }, { key: "isHeistable", value:
 
     function isHeistable() {
-      return (
-        (0,external_kolmafia_namespaceObject.toInt)((0,external_kolmafia_namespaceObject.getProperty)("_catBurglarCharge")) >= 11 &&
-        Object.keys((0,external_kolmafia_namespaceObject.heistTargets)()).find(
-        (k) => k.toLowerCase() == "mountain man") !=
-        null);
+      if ((0,external_kolmafia_namespaceObject.toInt)((0,external_kolmafia_namespaceObject.getProperty)("_catBurglarCharge")) < 11) {
+        return false;
+      }
 
+      var foundOre = Object.keys((0,external_kolmafia_namespaceObject.heistTargets)()).find(
+      (k) => k.toLowerCase() == "mountain man");
+
+
+      // Get out of the choice
+      (0,external_kolmafia_namespaceObject.visitUrl)("main.php");
+
+      return foundOre != null;
     } }, { key: "run", value:
 
     function run(path) {
@@ -32693,7 +32710,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "bf10981";
+var lastCommitHash = "2993d9a";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
