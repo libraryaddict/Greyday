10cd1d6e26ac3dd560a2fc7dd07ded20f6a2714e
Build Script <>
2022-11-08 04:11:17 +0000

    Build Mafia files for commit 8e4f6bea9a1015d7611e98f6715f9677f3465247

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -32730,11 +32730,22 @@ var GreyAdventurer = /*#__PURE__*/function () {function GreyAdventurer() {GreyAd
           }
         }
 
-        replaceWith.push.apply(replaceWith, GreyAdventurer_toConsumableArray(this.adventureFinder.getRecommendedFamiliars()));
+        // We want cat burglar to always be charged first
+        var burglar = external_kolmafia_namespaceObject.Familiar.get("Cat Burglar");
+        var recced = this.adventureFinder.getRecommendedFamiliars();
 
+        // If burglar wants to be charged, add it to the list
+        if (recced.includes(burglar)) {
+          replaceWith.push(burglar);
+        }
+
+        // If we want the recipe, add cookbat
         if (GreySettings.greyCookbatRecipe && !hasCookbatRecipe()) {
           replaceWith.push(external_kolmafia_namespaceObject.Familiar.get("Cookbookbat"));
         }
+
+        // Add every other fam, exclude burglar.
+        replaceWith.push.apply(replaceWith, GreyAdventurer_toConsumableArray(recced.filter(function (f) {return f != burglar;})));
 
         var robor = external_kolmafia_namespaceObject.Familiar.get("Robortender");
         var doRobor =
@@ -33070,7 +33081,7 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "64863e8";
+var lastCommitHash = "8e4f6be";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it["return"] != null) it["return"]();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
