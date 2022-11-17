29a0cc06b26e695ac008dd618a22cf86468a7191
Build Script <>
2022-11-17 10:46:03 +0000

    Build Mafia files for commit 1afdf9ff65e64b4694624d717851311d6e91113e

--- relay/relay_Greyday.js
+++ relay/relay_Greyday.js
@@ -2540,7 +2540,7 @@ function getGreySettings() {
   var greyGrabZapWand = {
     name: "greyGrabZapWand",
     description:
-    "Should the script grab the zap wand? This generally adds another 5-6 turns to the run",
+    "Should the script grab the zap wand? This generally adds another 5-6 turns to the run. You'll need this if you don't have enough key sources",
     valid: isBoolean,
     "default": false
   };
