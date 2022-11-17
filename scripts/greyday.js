de42bd52b0bc7c1916dc57b4106f68ab409bbf75
Build Script <>
2022-11-17 06:53:25 +0000

    Build Mafia files for commit c267b197c511321ac99867dc5562d033be789b7c

--- scripts/greyday.js
+++ scripts/greyday.js
@@ -33592,9 +33592,10 @@ var GreyTimings = /*#__PURE__*/function () {function GreyTimings() {GreyTimings_
       return "".concat(hours, ":").concat(minutes, ":").concat(seconds);
     } }]);return GreyTimings;}();
 ;// CONCATENATED MODULE: ./src/_git_commit.ts
-var lastCommitHash = "1d0a9d2";
+var lastCommitHash = "c267b19";
 ;// CONCATENATED MODULE: ./src/GreyYouMain.ts
 function GreyYouMain_createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = GreyYouMain_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it["return"] != null) it["return"]();} finally {if (didErr) throw err;}} };}function GreyYouMain_unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return GreyYouMain_arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return GreyYouMain_arrayLikeToArray(o, minLen);}function GreyYouMain_arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function GreyYouMain_classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function GreyYouMain_defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function GreyYouMain_createClass(Constructor, protoProps, staticProps) {if (protoProps) GreyYouMain_defineProperties(Constructor.prototype, protoProps);if (staticProps) GreyYouMain_defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function GreyYouMain_defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
+
 
 
 
@@ -33675,6 +33676,14 @@ GreyYouMain = /*#__PURE__*/function () {function GreyYouMain() {GreyYouMain_clas
       (0,external_kolmafia_namespaceObject.print)("");
       (0,external_kolmafia_namespaceObject.printHtml)(
       "<div style=\"text-align: center;\" color='blue'>======================</div>");
+
+
+      this.clickThis();
+    } }, { key: "clickThis", value:
+
+    function clickThis() {
+      (0,external_kolmafia_namespaceObject.printHtml)(
+      "It's easiest to use the <a href='relay_Greyday.js?relay=true'><u>relay page</u></a>");
 
     } }, { key: "doHelp", value:
 
@@ -33789,13 +33798,18 @@ GreyYouMain = /*#__PURE__*/function () {function GreyYouMain() {GreyYouMain_clas
         return;
       }
 
-      var banishedAbsorbs = AbsorbsProvider.loadAbsorbs(true).filter(function (absorb) {return (
-          (0,external_kolmafia_namespaceObject.isBanished)(absorb.monster));});
+      var banishedAbsorbs = AbsorbsProvider.loadAbsorbs(true).filter(
+      function (absorb) {return (
+          getBanished().find(
+          function (b) {return (
+              b.banisher.type == BanishType.ICE_HOUSE &&
+              b.monster == absorb.monster);}) !=
+          null);});
 
 
       if (banishedAbsorbs.length > 0) {
         (0,external_kolmafia_namespaceObject.print)(
-        "Please note that the following has been banished, if this is a persistant banish (Eg, Ice House). Then we cannot absorb said monster. Monsters: " +
+        "Your Ice House contains a monster we'd like to absorb! Please release them.. " +
         banishedAbsorbs.map(function (a) {return a.monster;}).join(", "),
         "red");
 
